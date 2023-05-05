df4032a40b093002968e92ddc1218400f80c8545
JanSeliv <janseliw@gmail.com>
2023-05-04 22:53:38 -0400

    Added 'Instanced Static Mesh Converter' plugin (is empty for now)

---
+++ Source/InstancedStaticMeshConverter/InstancedStaticMeshConverter.Build.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Yevhenii Selivanov.
+
+using UnrealBuildTool;
+
+public class InstancedStaticMeshConverter : ModuleRules
+{
+	public InstancedStaticMeshConverter(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		CppStandard = CppStandardVersion.Latest;
+
+		PublicDependencyModuleNames.AddRange(new[]
+			{
+				"Core"
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(new[]
+			{
+				"CoreUObject", "Engine", "Slate", "SlateCore" // Core
+			}
+		);
+	}
+}
---
+++ Source/InstancedStaticMeshConverter/Private/InstancedStaticMeshConverterModule.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Yevhenii Selivanov.
+
+#include "InstancedStaticMeshConverterModule.h"
+
+#define LOCTEXT_NAMESPACE "FInstancedStaticMeshConverterModule"
+
+void FInstancedStaticMeshConverterModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FInstancedStaticMeshConverterModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FInstancedStaticMeshConverterModule, InstancedStaticMeshConverter)
