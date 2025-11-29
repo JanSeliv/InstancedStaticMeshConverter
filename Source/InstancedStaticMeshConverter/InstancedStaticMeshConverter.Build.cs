9b9528dc7029c4d1e3f3fdb2437a6343d5b3f370
JanSeliv <janseliw@gmail.com>
2025-11-28 22:05:39 -0500

    [Bug][UE5.7] Fixed compilation error after engine update on MacOS: do not force cppstandard, but use the platform default version

--- Source/InstancedStaticMeshConverter/InstancedStaticMeshConverter.Build.cs
+++ Source/InstancedStaticMeshConverter/InstancedStaticMeshConverter.Build.cs
@@ -7,7 +7,6 @@ public class InstancedStaticMeshConverter : ModuleRules
 	public InstancedStaticMeshConverter(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		CppStandard = CppStandardVersion.Latest;
 		CppCompileWarningSettings.NonInlinedGenCppWarningLevel = WarningLevel.Error;
 
 		PublicDependencyModuleNames.AddRange(new[]
